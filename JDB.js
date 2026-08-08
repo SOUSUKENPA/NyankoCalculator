84194390466bcf2eb72bedfded80c5a94ff50b62
SOUSUKENPA <krzenkyo@yahoo.co.jp>
2026-08-08 18:47:57 +0900

    変更

--- JDB.js
+++ JDB.js
@@ -1,2 +1,24 @@
 alert("jdb.js読み込まれた！");
 console.log("JDBロード完了");
+
+console.log("ユニット数:", unit_data1_ja.length);
+
+function getCatData(name) {
+
+    for (const unit of unit_data1_ja) {
+
+        for (const form of unit.forms) {
+
+            if (form.name === name) {
+
+                console.log("見つかった:", form);
+
+                return form;
+            }
+        }
+    }
+
+    console.log("見つからない:", name);
+
+    return null;
+}
